65f895ca86c806147ad95019b6283098fd5bd2bb
Fred31-Ak <52544654+Fred31-Ak@users.noreply.github.com>
2022-01-25 10:12:55 +0000

    fetch from api

--- src/pages/staffIPD/IPDStaff.jsx
+++ src/pages/staffIPD/IPDStaff.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
-import './istaff.css'
+import './istaff.css';
+import { DataGrid } from "@material-ui/data-grid";
+import { useState, useEffect } from 'react';
+import { DeleteOutline } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 export default function IPDStaff() {
-    return (
-        <div className='staffList'>
-            List
-        </div>
-    )
+
+    const handleDelete = (id) => {
+        setTableData(tableData.filter(item=>item.id !== id));
+    
+         };
+    
+      const columns = [
+        {field: 'name', headerName: 'Name', width: 150},
+        {field: 'age', headerName: 'Age', width: 100},
+        {field: 'position', headerName: 'Position', width: 160},
+    
+        {
+                  field: "action",
+                  headerName: "Action",
+                  width: 150,
+                  renderCell: (params) => {
+                    return(
+        
+                      <>
+                      <Link to={"/staff/"+params.row.doctorId}>
+                      <button className="istaffListEdit">Edit</button>
+                      </Link>
+                          <DeleteOutline className="istaffListDelete" onClick={() => handleDelete(params.row.doctorId)}/>
+                      </>
+                    );
+                  }
+                }
+      ]
+    
+    
+        const [tableData, setTableData] = useState([]);
+    
+        useEffect(() => {
+          fetch("http://localhost:61114/api/doctors/getalloutdoctors").then((data) => data.json())
+          .then((data)=> setTableData(data))
+    
+    
+        }, [])
+      
+      
+       
+               
+        return (
+            <div className='staffList'>
+              <div className='listHeading'>In-Patients' Department
+              <br/><span className='subHeading'>Medical Staff</span></div>
+                       <DataGrid getRowId={row => row.doctorId} rows={tableData} 
+                       disableSelectionOnClick 
+                       columns={columns} 
+                        pageSize={12}
+                       checkboxSelection />
+    
+             </div>
+    
+        );
 }
+  
--- src/pages/staffOPD/OPDStaff.jsx
+++ src/pages/staffOPD/OPDStaff.jsx
@@ -1,77 +1,65 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './ostaff.css';
 import { DataGrid } from "@material-ui/data-grid";
-import { ostaffRows } from "../../dummyData";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { DeleteOutline } from "@mui/icons-material";
+import {  DeleteOutline } from "@mui/icons-material";
 
 export default function OPDStaff() {
-    const [data, setData] = useState(ostaffRows);
 
-    const handleDelete = (id) => {
-        setData(data.filter(item=>item.id !== id));
-      };
+   const handleDelete = (id) => {
+    setTableData(tableData.filter(item=>item.id !== id));
 
-      const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'name', headerName: 'Name', width: 160, renderCell: (params) =>{
-            return (
-                <div>
-                    {params.row.name}
-                </div>
-            )
-        } },
-        { field: 'lastName', headerName: 'Last Name', width: 160 },
-        {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 90,
-        },
-        {
-            field: 'position',
-            headerName: 'Position',
+     };
+
+  const columns = [
+    {field: 'name', headerName: 'Name', width: 150},
+    {field: 'age', headerName: 'Age', width: 100},
+    {field: 'position', headerName: 'Position', width: 160},
+
+    {
+              field: "action",
+              headerName: "Action",
+              width: 150,
+              renderCell: (params) => {
+                return(
     
-            width: 140,
-          },
-        // {
-        //   field: 'position',
-        //   headerName: 'Position',
-        //   description: 'This column has a value getter and is not sortable.',
-        //   sortable: false,
-        //   width: 160,
-        //   valueGetter: (params) =>
-        //     `${params.getValue(params.id, 'position') || ''} ${
-        //       params.getValue(params.id, 'lastName') || ''
-        //     }`,
-        // },
+                  <>
+                  <Link to={"/staff/"+params.row.doctorId}>
+                  <button className="ostaffListEdit">Edit</button>
+                  </Link>
+                      <DeleteOutline className="ostaffListDelete" onClick={() => handleDelete(params.row.doctorId)}/>
+                  </>
+                );
+              }
+            }
+  ]
 
-        {
-          field: "action",
-          headerName: "Action",
-          width: 150,
-          renderCell: (params) => {
-            return(
 
-              <>
-              <Link to={"/staff/"+params.row.id}>
-              <button className="ostaffListEdit">Edit</button>
-              </Link>
-                  <DeleteOutline className="ostaffListDelete" onClick={() => handleDelete(params.row.id)}/>
-              </>
-            );
-          }
-        }
-      ];
-      
+    const [tableData, setTableData] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:61114/api/doctors/getalldoctors").then((data) => data.json())
+      .then((data)=> setTableData(data))
+
+
+    }, [])
+  
+  
+
+  
+           
     return (
         <div className='staffList'>
-                  <DataGrid rows={data} 
-                  disableSelectionOnClick 
-                  columns={columns} 
-                  checkboxSelection />
+          <div className='listHeading'>Out-Patients' Department
+          <br/><span className='subHeading'>Medical Staff</span></div>
+                   <DataGrid getRowId={row => row.doctorId} rows={tableData} 
+                   disableSelectionOnClick                    
+                   columns={columns} 
+                   checkboxSelection />
 
-        </div>
-    )
+         </div>
+
+    );
+    
 }
